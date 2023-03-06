6a6f3f10d53e902f3edc968bcef8db9de098591f
seth <getchoo@tuta.io>
2023-03-06 09:34:06 -0500

    material-color-utilities: update meta info

--- pkgs/material-color-utilities.nix
+++ pkgs/material-color-utilities.nix
@@ -10,5 +10,14 @@ python3Packages.buildPythonPackage rec {
     inherit version;
     sha256 = "sha256-PG8C585wWViFRHve83z3b9NijHyV+iGY2BdMJpyVH64=";
   };
-  propagatedBuildInputs = with python3Packages; [pillow regex poetry-core];
+
+  nativeBuildInputs = with python3Packages; [poetry-core];
+  propagatedBuildInputs = with python3Packages; [pillow regex];
+
+  meta = with lib; {
+    description = "Python port of material-color-utilities used for Material You colors";
+    license = licenses.asl20;
+    maintainers = with maintainers; [getchoo];
+    platforms = platforms.all;
+  };
 }
