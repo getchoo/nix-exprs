68d93903b9b35914118f7a9aa01ae7d836272206
seth <getchoo@tuta.io>
2023-06-27 03:43:29 -0400

    pkgs: refactor vim-just

--- pkgs/vim-just.nix
+++ pkgs/vim-just.nix
@@ -1,18 +1,15 @@
 {
   fetchFromGitHub,
   vimUtils,
-}: let
-  inherit (builtins) substring;
-  inherit (vimUtils) buildVimPluginFrom2Nix;
-  rev = "18246a7cbb8b82c0682e9a49039bb5c376b79a6c";
-in
-  buildVimPluginFrom2Nix {
-    pname = "vim-just";
-    version = substring 0 7 rev;
-    src = fetchFromGitHub {
-      owner = "NoahTheDuke";
-      repo = "vim-just";
-      inherit rev;
-      sha256 = "sha256-KNmt9Axpl/uRj4wKzWkjcvHKCdjShsBjEVK1kFQ2VzE=";
-    };
-  }
+}:
+vimUtils.buildVimPluginFrom2Nix {
+  pname = "vim-just";
+  version = "2023-04-21";
+
+  src = fetchFromGitHub {
+    owner = "NoahTheDuke";
+    repo = "vim-just";
+    rev = "9fc9a1afaa9e3567b25f4141a01f6172a1992a0b";
+    sha256 = "sha256-O3HCNOVlo3MAkTQw622n5KTUIVPZd4HJvOXyDZbEWXI=";
+  };
+}
