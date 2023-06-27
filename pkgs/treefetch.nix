dac411699f617a5f2f3866da1e4b85befe383600
seth <getchoo@tuta.io>
2023-06-27 03:40:50 -0400

    pkgs: refactor treefetch

--- pkgs/treefetch.nix
+++ pkgs/treefetch.nix
@@ -2,33 +2,26 @@
   lib,
   fetchFromGitHub,
   rustPlatform,
-}: let
-  commit = "02f65e11e1f23d5fa9e66335eb5ff4f2f6b01400";
-  inherit (rustPlatform) buildRustPackage;
-  inherit (lib) licenses maintainers platforms;
-  inherit (builtins) substring;
-in
-  buildRustPackage rec {
-    pname = "treefetch";
-    version = substring 0 7 commit;
+}:
+rustPlatform.buildRustPackage {
+  pname = "treefetch";
+  version = "2022-06-07";
 
-    src = fetchFromGitHub {
-      owner = "angelofallars";
-      repo = "treefetch";
-      rev = commit;
-      sha256 = "sha256-FDiulTit492KwV46A3qwjHQwzpjVJvIXTfTrMufXd5k=";
-    };
+  src = fetchFromGitHub {
+    owner = "angelofallars";
+    repo = "treefetch";
+    rev = "02f65e11e1f23d5fa9e66335eb5ff4f2f6b01400";
+    sha256 = "sha256-FDiulTit492KwV46A3qwjHQwzpjVJvIXTfTrMufXd5k=";
+  };
 
-    cargoLock = {
-      lockFile = "${src}/Cargo.lock";
-    };
+  cargoSha256 = "sha256-6Ne41Mo4Ixx5YpcgypvvA71p9clTqmNTN84kzhWfwJQ=";
 
-    meta = {
-      description = "A plant-based system fetch tool made with Rust.";
-      longDescription = "A comfy and fast system fetch tool made in Rust. Tested to be much faster than neofetch and pfetch.";
-      homepage = "https://github.com/angelofallars/treefetch";
-      license = licenses.gpl3;
-      #maintainers = with maintainers; [getchoo];
-      platforms = platforms.unix;
-    };
-  }
+  meta = with lib; {
+    description = "A plant-based system fetch tool made with Rust.";
+    longDescription = "A comfy and fast system fetch tool made in Rust. Tested to be much faster than neofetch and pfetch.";
+    homepage = "https://github.com/angelofallars/treefetch";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [getchoo];
+    platforms = platforms.unix;
+  };
+}
