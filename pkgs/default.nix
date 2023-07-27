8912c18891adbd676655965981acb52ba00f2af9
seth <getchoo@tuta.io>
2023-07-27 18:48:56 -0400

    flake: simplify checks/packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,12 +1,12 @@
-final: _: let
-  inherit (final) callPackage;
+pkgs: let
+  inherit (pkgs) callPackage;
 in {
   # original packages
   cfspeedtest = callPackage ./cfspeedtest.nix {};
   check-pr = callPackage ./check-pr.nix {};
   huion = callPackage ./huion.nix {};
   mommy = callPackage ./mommy.nix {};
-  theseus = callPackage ./theseus.nix {inherit (final.nodePackages) pnpm;};
+  theseus = callPackage ./theseus.nix {inherit (pkgs.nodePackages) pnpm;};
   treefetch = callPackage ./treefetch.nix {};
   swhkd = callPackage ./swhkd {};
   vim-just = callPackage ./vim-just.nix {};
