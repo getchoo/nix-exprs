210460674a053e0c66ed3f43b0a19f6f04317678
seth <getchoo@tuta.io>
2023-07-30 20:23:23 -0400

    pkgs/xwaylandvideobridge: unstable-2023-07-26 -> unstable-2023-07-30
    Diff: https://invent.kde.org/system/xwaylandvideobridge/-/compare/014111b4db6298968204fd56d5ce6f691137bf90...1bc99dec7d44443bcc91da8425facd4d5d41b485

--- pkgs/xwaylandvideobridge.nix
+++ pkgs/xwaylandvideobridge.nix
@@ -23,14 +23,14 @@
 in
   stdenv.mkDerivation rec {
     pname = "xwaylandvideobridge";
-    version = "unstable-2023-07-26";
+    version = "unstable-2023-07-30";
 
     src = fetchFromGitLab {
       domain = "invent.kde.org";
       owner = "system";
       repo = pname;
-      rev = "014111b4db6298968204fd56d5ce6f691137bf90";
-      sha256 = "sha256-6ro5Y5GSiodzbsiPQls4IAOYGLcLE7j+0eumpW6+HeU=";
+      rev = "1bc99dec7d44443bcc91da8425facd4d5d41b485";
+      sha256 = "sha256-An7g4eXCUcJMAf7ZTj8gz+MDKFDwpLvyxOFcZnJlSKw=";
     };
 
     nativeBuildInputs = [
