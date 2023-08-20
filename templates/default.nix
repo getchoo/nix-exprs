6c0a1f9a2242bec3a024f3899265a100700d8932
seth <getchoo@tuta.io>
2023-08-20 18:56:53 -0400

    templates/nixos: init

--- templates/default.nix
+++ templates/default.nix
@@ -9,6 +9,7 @@
     in {
       basic = mkTemplate "basic" "minimal boilerplate for my flakes";
       full = mkTemplate "full" "big template for complex flakes (using flake-parts)";
+      nixos = mkTemplate "nixos" "minimal boilerplate for flake-based nixos configuration";
     };
   };
 }
---
+++ templates/nixos/flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "my cool flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {nixpkgs, ...} @ inputs: {
+    nixosConfigurations."myHostname" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [./configuration.nix];
+      specialArgs = inputs;
+    };
+  };
+}
