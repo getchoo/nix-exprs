29a12662a1b96f140d81b14e1ba32df2d8779e15
seth <getchoo@tuta.io>
2023-09-01 17:14:42 -0400

    flake: use scopes to manage packages output

--- pkgs/all-packages.nix
+++ pkgs/all-packages.nix
@@ -1,28 +1,32 @@
-{
-  final ? {},
-  prev,
-}: let
+final: prev: let
   inherit (prev) callPackage;
-  callPackage' =
-    final.callPackage or prev.lib.callPackageWith (prev // packages);
+in {
+  cfspeedtest = callPackage ./cfspeedtest.nix {};
 
-  packages = {
-    cfspeedtest = callPackage ./cfspeedtest.nix {};
-    check-pr = callPackage ./check-pr.nix {};
-    fastfetch = callPackage ./fastfetch.nix {};
-    huion = callPackage ./huion.nix {};
-    klassy = prev.libsForQt5.callPackage ./klassy.nix {};
-    mommy = callPackage ./mommy.nix {};
-    nixgc = callPackage ./nixgc.nix {};
-    modrinth-app-unwrapped = callPackage ./modrinth-app {
-      inherit (prev.nodePackages) pnpm;
-      inherit (prev.darwin.apple_sdk.frameworks) CoreServices Security WebKit;
-    };
-    modrinth-app = callPackage' ./modrinth-app/wrapper.nix {};
-    treefetch = callPackage ./treefetch.nix {};
-    swhkd = callPackage ./swhkd {};
-    vim-just = callPackage ./vim-just.nix {};
-    xwaylandvideobridge = callPackage ./xwaylandvideobridge.nix {};
+  check-pr = callPackage ./check-pr.nix {};
+
+  fastfetch = callPackage ./fastfetch.nix {};
+
+  huion = callPackage ./huion.nix {};
+
+  klassy = prev.libsForQt5.callPackage ./klassy.nix {};
+
+  mommy = callPackage ./mommy.nix {};
+
+  nixgc = callPackage ./nixgc.nix {};
+
+  modrinth-app-unwrapped = final.callPackage ./modrinth-app {
+    inherit (prev.nodePackages) pnpm;
+    inherit (prev.darwin.apple_sdk.frameworks) CoreServices Security WebKit;
   };
-in
-  packages
+
+  modrinth-app = final.callPackage ./modrinth-app/wrapper.nix {};
+
+  treefetch = callPackage ./treefetch.nix {};
+
+  swhkd = callPackage ./swhkd {};
+
+  vim-just = callPackage ./vim-just.nix {};
+
+  xwaylandvideobridge = callPackage ./xwaylandvideobridge.nix {};
+}
