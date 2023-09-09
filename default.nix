e836acc0832f599de9b74224703cad06eafd38d5
seth <getchoo@tuta.io>
2023-09-09 17:52:13 -0400

    flake: fix default.nix compat

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 (import
   (fetchTarball {
     url = "https://github.com/edolstra/flake-compat/archive/35bb57c0c8d8b62bbfd284272c928ceb64ddbde9.tar.gz";
-    hash = "sha256-4gtG9iQuiKITOjNQQeQIpoIB6b16fm+504Ch3sNKLd8=";
-  } {src = ./.;}))
+    sha256 = "sha256-4gtG9iQuiKITOjNQQeQIpoIB6b16fm+504Ch3sNKLd8=";
+  }) {src = ./.;})
 .defaultNix
