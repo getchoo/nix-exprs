3e6dca9d8aa30b6b44bb5fbb8aa59fcb7eb4fe0c
seth <getchoo@tuta.io>
2023-09-14 08:40:27 -0400

    pkgs/cfspeedtest: 1.0.3 -> 1.1.1
    Diff: https://github.com/code-inflation/cfspeedtest/compare/v1.0.3...v1.1.1

--- pkgs/cfspeedtest.nix
+++ pkgs/cfspeedtest.nix
@@ -5,13 +5,13 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "cfspeedtest";
-  version = "1.0.3";
+  version = "1.1.1";
 
   src = fetchFromGitHub {
     owner = "code-inflation";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-FgxSx0atSx3FccgjiO8OZ4j4IjkTpToGSViZyS3s8Cw=";
+    sha256 = "sha256-YF/jf1TzNW1QswNefQ4qKeXDyjFoN9/AWcjoeENCgvc=";
   };
 
   cargoLock = {
