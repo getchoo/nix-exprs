c2bc4a4b5dae96f7704e2610f7aa72278ea58cfc
seth <getchoo@tuta.io>
2023-10-07 10:27:47 -0400

    vim-just: unstable-2023-08-02 -> unstable-2023-10-06
    Diff: https://github.com/NoahTheDuke/vim-just/compare/927b41825b9cd07a40fc15b4c68635c4b36fa923...fbcfcf96cf7c67cf0d4ad15b7af5069f65440c4f

--- pkgs/vim-just/default.nix
+++ pkgs/vim-just/default.nix
@@ -4,12 +4,12 @@
 }:
 vimUtils.buildVimPlugin {
   pname = "vim-just";
-  version = "unstable-2023-08-02";
+  version = "unstable-2023-10-06";
 
   src = fetchFromGitHub {
     owner = "NoahTheDuke";
     repo = "vim-just";
-    rev = "927b41825b9cd07a40fc15b4c68635c4b36fa923";
-    sha256 = "sha256-BmxYWUVBzTowH68eWNrQKV1fNN9d1hRuCnXqbEagRoY=";
+    rev = "fbcfcf96cf7c67cf0d4ad15b7af5069f65440c4f";
+    sha256 = "sha256-gCljLZcSbN738bNMQDFF+N5kVC7+Q3tsB5FlozFbiqs=";
   };
 }
