0fc48fd45f756534648d989f5a2c4f3060464218
seth <getchoo@tuta.io>
2023-12-24 04:20:53 -0500

    pkgs: auto-import directories *and* files

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,31 +1,31 @@
 final: prev:
 with prev; let
-  # directories are mapped to packages here for convenience sake
+  # files/directories are mapped to packages here for convenience sake
   imported = lib.pipe ./. [
     builtins.readDir
 
     (
       lib.filterAttrs (
-        name: type: !(lib.hasPrefix "_" name) && type == "directory"
+        name: _: !(lib.hasPrefix "_" name) && name != "default.nix"
       )
     )
 
     (
-      lib.mapAttrs (
-        file: _: callPackage ./${file} {}
+      lib.mapAttrs' (
+        file: _: lib.nameValuePair (lib.removeSuffix ".nix" file) (callPackage ./${file} {})
       )
     )
   ];
 in
   imported
   // {
-    clang-tidy-sarif = callPackage ./clang-tidy-sarif {inherit (final) clang-tidy-sarif;};
-    clippy-sarif = callPackage ./clippy-sarif {inherit (final) clippy-sarif;};
-    hadolint-sarif = callPackage ./hadolint-sarif {inherit (final) hadolint-sarif;};
-    sarif-fmt = callPackage ./sarif-fmt {inherit (final) sarif-fmt;};
-    shellcheck-sarif = callPackage ./shellcheck-sarif {inherit (final) shellcheck-sarif;};
+    clang-tidy-sarif = callPackage ./clang-tidy-sarif.nix {inherit (final) clang-tidy-sarif;};
+    clippy-sarif = callPackage ./clippy-sarif.nix {inherit (final) clippy-sarif;};
+    hadolint-sarif = callPackage ./hadolint-sarif.nix {inherit (final) hadolint-sarif;};
+    sarif-fmt = callPackage ./sarif-fmt.nix {inherit (final) sarif-fmt;};
+    shellcheck-sarif = callPackage ./shellcheck-sarif.nix {inherit (final) shellcheck-sarif;};
 
-    klassy = libsForQt5.callPackage ./klassy {};
+    klassy = libsForQt5.callPackage ./klassy.nix {};
 
     modrinth-app-unwrapped = callPackage ./modrinth-app {
       inherit (final.nodePackages or prev.nodePackages) pnpm;
