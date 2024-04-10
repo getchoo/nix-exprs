b62ecb140da824fdb2425358ec1ab0b0b80f8e6c
seth <getchoo@tuta.io>
2024-04-10 19:16:57 -0400

    pkgsFrom -> packagesFromDirectoryRecursive
    i had no idea this was a thing. thanks aemogie

--- overlay.nix
+++ overlay.nix
@@ -1,36 +1,24 @@
 final: prev: let
-  inherit (prev) lib callPackage;
-
-  # use `callPackage` on all expressions in a directory
-  pkgsFrom = dir:
-    lib.mapAttrs' (
-      name: _:
-        lib.nameValuePair (lib.removeSuffix ".nix" name) (callPackage "${dir}/${name}" {})
-    ) (builtins.readDir dir);
+  inherit (prev) callPackage;
 in
-  pkgsFrom ./pkgs
+  prev.lib.packagesFromDirectoryRecursive {
+    inherit (prev) callPackage;
+    directory = ./pkgs;
+  }
   // {
     clang-tidy-sarif = callPackage ./pkgs/clang-tidy-sarif.nix {inherit (final) clang-tidy-sarif;};
     clippy-sarif = callPackage ./pkgs/clippy-sarif.nix {inherit (final) clippy-sarif;};
     hadolint-sarif = callPackage ./pkgs/hadolint-sarif.nix {inherit (final) hadolint-sarif;};
     sarif-fmt = callPackage ./pkgs/sarif-fmt.nix {inherit (final) sarif-fmt;};
     shellcheck-sarif = callPackage ./pkgs/shellcheck-sarif.nix {inherit (final) shellcheck-sarif;};
+
     flat-manager = callPackage ./pkgs/flat-manager.nix {inherit (final) flat-manager;};
     flat-manager-client = callPackage ./pkgs/flat-manager-client.nix {inherit (final) flat-manager;};
+
     klassy = prev.libsForQt5.callPackage ./pkgs/klassy.nix {};
 
-    modrinth-app-unwrapped = callPackage ./pkgs/modrinth-app {
-      inherit (final.nodePackages) pnpm;
+    modrinth-app-unwrapped = callPackage ./pkgs/modrinth-app-unwrapped/package.nix {inherit (final) modrinth-app-unwrapped;};
+    modrinth-app = callPackage ./pkgs/modrinth-app.nix {inherit (final) modrinth-app-unwrapped;};
 
-      inherit
-        (final.darwin.apple_sdk.frameworks)
-        AppKit
-        CoreServices
-        Security
-        WebKit
-        ;
-    };
-
-    modrinth-app = callPackage ./pkgs/modrinth-app/wrapper.nix {inherit (final) modrinth-app-unwrapped;};
-    papa = callPackage ./pkgs/papa {inherit (final) papa;};
+    papa = callPackage ./pkgs/papa/package.nix {inherit (final) papa;};
   }
