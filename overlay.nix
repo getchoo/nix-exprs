1183d399afbe83ca24f3a59d4fe02c9a025e651c
seth <getchoo@tuta.io>
2024-05-22 04:06:24 -0400

    treewide: enforce <dir>/package.nix

--- overlay.nix
+++ overlay.nix
@@ -6,13 +6,13 @@ in
     directory = ./pkgs;
   }
   // {
-    clang-tidy-sarif = callPackage ./pkgs/clang-tidy-sarif.nix {inherit (final) clang-tidy-sarif;};
-    clippy-sarif = callPackage ./pkgs/clippy-sarif.nix {inherit (final) clippy-sarif;};
-    hadolint-sarif = callPackage ./pkgs/hadolint-sarif.nix {inherit (final) hadolint-sarif;};
-    sarif-fmt = callPackage ./pkgs/sarif-fmt.nix {inherit (final) sarif-fmt;};
+    clang-tidy-sarif = callPackage ./pkgs/clang-tidy-sarif/package.nix {inherit (final) clang-tidy-sarif;};
+    clippy-sarif = callPackage ./pkgs/clippy-sarif/package.nix {inherit (final) clippy-sarif;};
+    hadolint-sarif = callPackage ./pkgs/hadolint-sarif/package.nix {inherit (final) hadolint-sarif;};
+    sarif-fmt = callPackage ./pkgs/sarif-fmt/package.nix {inherit (final) sarif-fmt;};
 
-    flat-manager = callPackage ./pkgs/flat-manager.nix {inherit (final) flat-manager;};
-    flat-manager-client = callPackage ./pkgs/flat-manager-client.nix {inherit (final) flat-manager;};
+    flat-manager = callPackage ./pkgs/flat-manager/package.nix {inherit (final) flat-manager;};
+    flat-manager-client = callPackage ./pkgs/flat-manager-client/package.nix {inherit (final) flat-manager;};
 
     papa = callPackage ./pkgs/papa/package.nix {inherit (final) papa;};
   }
