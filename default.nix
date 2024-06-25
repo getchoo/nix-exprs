b9d91d6986c5638f534cd0241ddc29441b132029
seth <getchoo@tuta.io>
2024-06-24 23:45:45 -0400

    clang-tidy-sarif,hadolint-sarif,sarif-fmt: drop
    these have been merged into nixpkgs

--- default.nix
+++ default.nix
@@ -23,10 +23,7 @@ in
         directory = ./pkgs;
       }
       // {
-        clang-tidy-sarif = callPackage ./pkgs/clang-tidy-sarif/package.nix {inherit (final) clang-tidy-sarif;};
         clippy-sarif = callPackage ./pkgs/clippy-sarif/package.nix {inherit (final) clippy-sarif;};
-        hadolint-sarif = callPackage ./pkgs/hadolint-sarif/package.nix {inherit (final) hadolint-sarif;};
-        sarif-fmt = callPackage ./pkgs/sarif-fmt/package.nix {inherit (final) sarif-fmt;};
 
         flat-manager = callPackage ./pkgs/flat-manager/package.nix {inherit (final) flat-manager;};
         flat-manager-client = callPackage ./pkgs/flat-manager-client/package.nix {inherit (final) flat-manager;};
